ecae4f6d5a69aeabc9e9b609c03489ecd64013e7
cwaller33 <59543955+cwaller33@users.noreply.github.com>
2025-04-17 19:20:52 -0400

    Made minimal layout without styling.

---
+++ src/app/feed/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./feed.module.css";
+import Link from "next/link";
+
+export default function Feed() 
+{
+  return (
+    <div className={styles.container}>
+      <h1> Feed </h1>
+    </div>
+  );
+}
