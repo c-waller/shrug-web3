96ab1058d852819c7d00a2836cbfdf8a4861ae5b
cwaller33 <59543955+cwaller33@users.noreply.github.com>
2025-04-19 23:50:38 -0400

    Changed up the front page a little. Added tagline and "shrug" to title.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home()
 {
   return (
     <div className={styles.container}>
-      <h1> ¯\_(ツ)_/¯ </h1>
-      {/* <MainButton> Sign Up </MainButton> */}
+      <h1 className={styles.title}> ¯\_(ツ)_/¯ Shrug </h1>
+      <p className={styles.tagline}> A quiet corner of the internet for whatever's on your mind. </p>
       <Link href={"/feed"}> <MainButton> Discover </MainButton> </Link>
     </div>
   );
