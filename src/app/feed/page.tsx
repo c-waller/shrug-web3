0aa3fc459f0f1754009c9fb09fc3b0ccff80708f
cwaller33 <59543955+cwaller33@users.noreply.github.com>
2025-04-20 23:16:36 -0400

    Changed #main in feed to an animated gradient, added nicks to shrugs.

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -4,66 +4,77 @@ import styles from "./feed.module.css";
 import NavBar from "../components/NavBar";
 import React from "react";
 
-// examples
+// these are examples!!!!!!!!!
 const shrugs = [
   {
-    title: "Shrugging Through It.",
-    content: "¯\\_(ツ)_/¯",
+    title: "",
+    content: "",
+    img: "https://media1.tenor.com/m/aACGz-xUHLUAAAAd/drake-drizzy.gif",
+    nick: "someone",
     date: "Apr 18, 2025",
     id: "q7fkp-hu24a"
   },
   {
-    title: "Brain Empty",
+    title: "",
     content: "No thoughts.",
     date: "Apr 18, 2025",
+    nick: "someone",
     id: "x3nvt-kg91z"
   },
   {
-    title: "Late Night Ramble",
-    content: `Sometimes I sit and think about how strange it is that we're just out here spinning on a rock in space, trying to make sense of everything through memes, caffeine, and random thoughts posted on the internet. This post has no real point—just wanted to test how long one could get without the site telling me to chill`,
+    title: "",
+    content: "this website sucks",
     date: "Apr 18, 2025",
+    nick: "eli",
     id: "m8czd-wl56e"
   },
   {
     title: "Note To Self",
     content: "Drink some water. Stand up. Breathe. You're doing fine.",
     date: "Apr 18, 2025",
+    nick: "someone",
     id: "a1xrf-vn83y"
   },
   {
-    title: "Code at 3AM",
+    title: "",
     content: `console.log("maybe this will work");\n// it didn't`,
     date: "Apr 18, 2025",
+    nick: "x",
     id: "z9hdp-rm70q"
   },
   {
-    title: "Random Observation",
+    title: "observation",
     content: "People who walk fast in airports probably run entire empires",
     date: "Apr 18, 2025",
+    nick: "someone",
     id: "k4tjm-uz33c"
   },
   {
     title: "I Miss Old Youtube",
     content: "bring back the weird stuff from 2009!!! i wanna see a potato singing again!!",
     date: "Apr 18, 2025",
+    nick: "someone",
     id: "v6gsl-yt28b"
   },
   {
-    title: "Untitled",
+    title: "",
     content: "i dont really have anything to say, just felt like existing here for a second",
     date: "Apr 18, 2025",
+    nick: "thethinker",
     id: "t2wqx-mk45n"
   },
   {
     title: "Dream Log 04.18.25",
     content: "I was riding a bike through the clouds, racing a bird that sounded like it was beatboxing. Woke up and my legs hurt.",
     date: "Apr 18, 2025",
+    nick: "someone",
     id: "r5bze-pc60v"
   },
   {
     title: "i still haven't found an internship",
     content: "am i cooked?",
     date: "Apr 18, 2025",
+    nick: "notacsmajor",
     id: "n0yjd-xq17s"
   }
 ];
@@ -82,9 +93,9 @@ export default function Feed()
               {shrug.content.split("\n").map((line, i) => (
                 <p key={i}>{line}</p>
               ))}
-            {/* <img src="https://media1.tenor.com/m/aACGz-xUHLUAAAAd/drake-drizzy.gif"></img> */}
+            {shrug.img ? <img src= {shrug.img}></img>: <></>}
             </div>
-            <p className={styles.metadata}> {shrug.date} </p>
+            <p className={styles.metadata}> {shrug.date} · {shrug.nick} </p>
           </article>
         ))}
         <div className={styles.footer}>
